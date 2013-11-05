c623fe8513b7d70407896425591e057a46634a32
Hanna Liebl <hanna@hannaliebl.com>
2013-11-04 21:59:02 -0800

    changing from google web services to google maps api to avoid differnt domains error

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 $(function() {
 	$("form#get-address").submit(function() {
-		var submitted_street = $("input#street").val();
-		var submitted_citystate = $("input#citystate").val();
-		var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?address=" + submitted_street + submitted_citystate + "&sensor=false";
 		$("ul#legislators li").remove();
-		$("h2").remove();
-		$.getJSON(geocodingAPI, function (json) {
-			var latitude = json.results[0].geometry.location.lat;
-			var longitude = json.results[0].geometry.location.lng;
-			$.get("http://congress.api.sunlightfoundation.com/legislators/locate?apikey=784bd7ba096644f588b91c2e918e6786&latitude=" + latitude + "&longitude=" + longitude)
-			.done(function(responseJSON) {
+ 		$("h2").remove();
+ 		$("span.text-danger").addClass('hidden');
+ 		var submitted_street = $("input#street").val();
+ 		var submitted_citystate = $("input#citystate").val();
+		var address = submitted_citystate + submitted_street;
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({'address': address}, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK)
+			{
+				var latitude = results[0].geometry.location.lat();
+    			var longitude = results[0].geometry.location.lng();
+    			$.get("http://congress.api.sunlightfoundation.com/legislators/locate?apikey=784bd7ba096644f588b91c2e918e6786&latitude=" + latitude + "&longitude=" + longitude)
+				.done(function(responseJSON) {
 	      		responseJSON.results.forEach(function(legislator) {
 	      			if (legislator.chamber === "senate") {
 	      				var title = "Senator";
@@ -28,12 +32,14 @@ $(function() {
 	        		$("ul#legislators").append("<li class='" + legislator_class + " " + legislator_party_class + "'>" + title + " " + legislator.first_name + " " + legislator.last_name + "," + " " + "(" + legislator.party + ")" + " " + "<a href='" + legislator.contact_form + "' class='btn btn-default contact'>Contact</a>" + "</li>");
 				});
 				$("ul#legislators").prepend("<h2>YOUR LEGISLATORS ARE:</h2>");
+
 			});
-
-		return false;
-
+			} else {
+				$("span.text-danger").removeClass('hidden');
+			}
 		});
 
 		return false;
 	});
 });
+
